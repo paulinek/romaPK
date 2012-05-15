f5059b1a4f3c3fd3fd24c96774d2a61f2b9fd7b6
Pauline Koh <z3180004@cse.unsw.edu.au>
2012-05-15 15:59:56 +1000

    encapsulated how many cards and how expensive into "cardFactory" class

---
+++ src/net/panda2/roma/game/CardFactory.java
@@ -0,0 +1,23 @@
+package net.panda2.roma.game;
+
+import net.panda2.roma.card.cards.*;
+
+public class CardFactory {
+
+	public static void createInitialCards(ViewableCardDeck d) {
+         d.addNcards(2, new Legat( 5, 2));
+        d.addNcards(1, new Sicarius(9,2));
+        d.addNcards(2, new Architectus(3,4));
+        d.addNcards(2, new Consiliarius(4,4));
+        d.addNcards(2, new Gladiator(6,5));
+        d.addNcards(1, new Mercator(7,2));
+        d.addNcards(2, new Consul(3,3));
+        d.addNcards(3, new Legionarius(4,5));
+        d.addNcards(1, new Nero(8,9));
+	    d.addNcards(2, new Haruspex(4,3));
+        d.addNcards(2, new Velites(5,3));
+        d.addNcards(2, new Essedum(6,3));
+        d.addNcards(2, new TribunusPlebis(5,5));
+        d.addNcards(2, new Centurio(9,5));
+    }
+}
